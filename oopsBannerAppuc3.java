21f3b91adf66041e3163fca0b4d791549882d997
darshanarul466-svg <darshanarul466@gmail.com>
2026-02-23 09:26:52 +0530

    Add files via upload

---
+++ oopsBannerAppuc3.java
@@ -0,0 +1,16 @@
+public class oopsBannerAppuc3 {
+
+    public static void main(String[] args) {
+
+        String g = "   ";
+
+        System.out.println(String.join(g, " *** ", " *** ", " ***** ", " ***** "));
+        System.out.println(String.join(g, "**  **", "**  **", "**  ** ", "**  ** "));
+        System.out.println(String.join(g, "**  **", "**  **", "**  ** ", "**  ** "));
+        System.out.println(String.join(g, "**  **", "**  **", " ***** ", " ***** "));
+        System.out.println(String.join(g, "**  **", "**  **", "**      ", "     **"));
+        System.out.println(String.join(g, "**  **", "**  **", "**      ", "**   **"));
+        System.out.println(String.join(g, " *** ", " *** ", "**      ", " ***** "));
+
+    }
+}
